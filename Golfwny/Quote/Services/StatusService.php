4c58f421ea1bcae993b35bee2a77a857546f5299
David VanScott <david.vanscott@gmail.com>
2014-12-21 14:40:50 -0500

    Add some statuses

--- Golfwny/Quote/Services/StatusService.php
+++ Golfwny/Quote/Services/StatusService.php
@@ -2,10 +2,41 @@
 
 class StatusService {
 
-	const RECEIVED			= 1;
-	const PENDING			= 2;
-	const SUBMITTER_REVIEW	= 3;
-	const ACCEPTED			= 4;
-	const REJECTED			= 5;
+	const PENDING		= 1;
+	const SUBMITTED		= 2;
+	const ESTIMATE		= 3;
+	const ACCEPTED		= 4;
+	const REJECTED		= 5;
+	const BOOKED		= 6;
+
+	public static function toString($value)
+	{
+		switch ($value)
+		{
+			case static::PENDING:
+				return "Incomplete";
+			break;
+
+			case static::SUBMITTED:
+				return "Awaiting Review";
+			break;
+
+			case static::ESTIMATE:
+				return "Estimate Sent";
+			break;
+
+			case static::ACCEPTED:
+				return "Estimate accepted";
+			break;
+
+			case static::REJECTED:
+				return "Estimate rejected";
+			break;
+
+			case static::BOOKED:
+				return "Booked";
+			break;
+		}
+	}
 
 }
