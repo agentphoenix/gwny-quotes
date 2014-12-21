2ad568f10a3e7105be69187f77ca89e694843e1b
David VanScott <david.vanscott@gmail.com>
2014-12-21 14:39:50 -0500

    Update the helpers file

---
+++ app/helpers.php
@@ -0,0 +1,91 @@
+<?php
+
+if ( ! function_exists('config'))
+{
+	function config($key)
+	{
+		return Config::get($key);
+	}
+}
+
+if ( ! function_exists('partial'))
+{
+	function partial($view, $data = false)
+	{
+		$view = View::make("partials.{$view}");
+
+		if ($data)
+		{
+			$view->with((array) $data);
+		}
+
+		return $view;
+	}
+}
+
+if ( ! function_exists('modal'))
+{
+	function modal(array $data = array())
+	{
+		return View::make('partials.modal')
+			->with('modalId', (array_key_exists('id', $data)) ? $data['id'] : false)
+			->with('modalHeader', (array_key_exists('header', $data)) ? $data['header'] : false)
+			->with('modalBody', (array_key_exists('body', $data)) ? $data['body'] : false)
+			->with('modalFooter', (array_key_exists('footer', $data)) ? $data['footer'] : false);
+	}
+}
+
+if ( ! function_exists('alert'))
+{
+	function alert($level, $message, $icon = false, $header = false)
+	{
+		return View::make('partials.alert')
+			->withType($level)
+			->withContent($message)
+			->withHeader($header)
+			->withIcon($icon);
+	}
+}
+
+if ( ! function_exists('flash'))
+{
+	function flash($level, $message)
+	{
+		return alert($level, $message);
+	}
+}
+
+if ( ! function_exists('label'))
+{
+	function label($level, $message)
+	{
+		return View::make('partials.label')
+			->withClass($level)
+			->withContent($message);
+	}
+}
+
+if ( ! function_exists('convertFileSize'))
+{
+	function convertFileSize($size)
+	{
+		switch ($size)
+		{
+			case ($size / 1073741824) > 1:
+				return round(($size/1073741824), 2) . "GB";
+			break;
+
+			case ($size / 1048576) > 1:
+				return round(($size/1048576), 2) . "MB";
+			break;
+
+			case ($size / 1024) > 1:
+				return round(($size/1024), 2) . "KB";
+			break;
+
+			default:
+				return $size . ' bytes';
+			break;
+		}
+	}
+}
