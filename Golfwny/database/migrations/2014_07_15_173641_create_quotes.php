a184b1b29ff012f6143b2e6e34560bc80a79cb99
David VanScott <david.vanscott@gmail.com>
2014-12-23 18:36:16 -0500

    Add percent fields

--- Golfwny/database/migrations/2014_07_15_173641_create_quotes.php
+++ Golfwny/database/migrations/2014_07_15_173641_create_quotes.php
@@ -27,6 +27,8 @@ class CreateQuotes extends Migration {
 			$table->date('departure');
 			$table->float('total')->nullable();
 			$table->float('deposit')->nullable();
+			$table->float('percent_package');
+			$table->float('percent_deposit');
 			$table->boolean('paid_deposit')->default((int) false);
 			$table->boolean('paid_total')->default((int) false);
 			$table->text('comments')->nullable();
