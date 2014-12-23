8e9839a647f87e464672d4075116ae2afd8100f1
David VanScott <david.vanscott@gmail.com>
2014-12-23 18:38:12 -0500

    Move the subnav into a partial

---
+++ Golfwny/Quote/views/partials/nav-sub.blade.php
@@ -0,0 +1,40 @@
+<a href="{{ route('home') }}" class="item visible-xs visible-sm">New Quote</a>
+<a href="{{ route('checkStatus') }}" class="item visible-xs visible-sm">Check the Status of a Quote</a>
+<a href="http://golfwny.com" class="item visible-xs visible-sm">Back to GolfWNY</a>
+
+<div class="header item"><i class="dollar icon"></i>Quotes</div>
+<a class="item">
+	Submitted
+	@if ($_countSubmitted > 0)
+		<div class="ui green label">{{ $_countSubmitted }}</div>
+	@endif
+</a>
+<a class="item">
+	Accepted
+	@if ($_countAccepted > 0)
+		<div class="ui green label">{{ $_countAccepted }}</div>
+	@endif
+</a>
+<a class="item">
+	Rejected
+	@if ($_countRejected > 0)
+		<div class="ui red label">{{ $_countRejected }}</div>
+	@endif
+</a>
+<a href="{{ route('admin.quote.index') }}" class="item">All Quotes</a>
+
+<div class="header item"><i class="align left icon"></i>Content</div>
+<a class="item">Regions</a>
+<a class="item">Courses</a>
+<a class="item">Hotels</a>
+<a class="item">Users</a>
+
+<div class="header item">
+	<i class="bar chart icon"></i>
+	Reports
+</div>
+<a class="item">Revenue</a>
+<a class="item">Course</a>
+<a class="item">Hotel</a>
+
+<a class="header item"><i class="announcement icon"></i>Survey Results</a>
