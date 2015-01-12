e398c153e5b6dd69b7b3dbda1c1a00519d462019
David VanScott <david.vanscott@gmail.com>
2015-01-12 15:47:18 -0500

    Add the login controller

---
+++ Golfwny/Quote/Controllers/LoginController.php
@@ -0,0 +1,20 @@
+<?php namespace Quote\Controllers;
+
+class LoginController extends BaseController {
+
+	public function index()
+	{
+		# code...
+	}
+
+	public function doLogin()
+	{
+		# code...
+	}
+
+	public function logout()
+	{
+		# code...
+	}
+
+}
