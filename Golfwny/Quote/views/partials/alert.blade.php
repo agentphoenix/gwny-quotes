6cef3a6e7996a5d0b908c7b617e2f70c2afb7fa9
David VanScott <david.vanscott@gmail.com>
2015-01-12 15:46:44 -0500

    Fix some issues with the alert partial

--- Golfwny/Quote/views/partials/alert.blade.php
+++ Golfwny/Quote/views/partials/alert.blade.php
@@ -1,12 +1,11 @@
 <div class="ui icon {{ $type }} message">
-	@if (isset($icon))
+	@if ($icon)
 		<i class="{{ $icon }} icon"></i>
 	@endif
 	<div class="content">
-		@if (isset($header))
+		@if ($header)
 			<div class="header">{{ $header }}</div>
 		@endif
-
 		<p>{{ $content }}</p>
 	</div>
 </div>
