4227f9398fe14d3ae9d1f5c4d5cf23469ab15156
David VanScott <david.vanscott@gmail.com>
2015-01-12 15:44:20 -0500

    Add password reminders framework

---
+++ Golfwny/Quote/Controllers/RemindersController.php
@@ -0,0 +1,63 @@
+<?php namespace Quote\Controllers;
+
+use App,
+	Hash,
+	Lang,
+	View,
+	Input,
+	Password,
+	Redirect,
+	Controller;
+
+class RemindersController extends Controller {
+
+	public function getRemind()
+	{
+		return View::make('pages.password.remind');
+	}
+
+	public function postRemind()
+	{
+		switch ($response = Password::remind(Input::only('email')))
+		{
+			case Password::INVALID_USER:
+				return Redirect::back()->with('error', Lang::get($response));
+
+			case Password::REMINDER_SENT:
+				return Redirect::back()->with('status', Lang::get($response));
+		}
+	}
+
+	public function getReset($token = null)
+	{
+		if (is_null($token)) App::abort(404);
+
+		return View::make('pages.password.reset')->with('token', $token);
+	}
+
+	public function postReset()
+	{
+		$credentials = Input::only(
+			'email', 'password', 'password_confirmation', 'token'
+		);
+
+		$response = Password::reset($credentials, function($user, $password)
+		{
+			$user->password = Hash::make($password);
+
+			$user->save();
+		});
+
+		switch ($response)
+		{
+			case Password::INVALID_PASSWORD:
+			case Password::INVALID_TOKEN:
+			case Password::INVALID_USER:
+				return Redirect::back()->with('error', Lang::get($response));
+
+			case Password::PASSWORD_RESET:
+				return Redirect::to('/');
+		}
+	}
+
+}
