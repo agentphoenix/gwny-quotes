80b124c5d68fc5d8400720a464297beb885a0cad
David VanScott <david.vanscott@gmail.com>
2015-01-17 13:56:36 -0500

    Handle hotel delete event

--- Golfwny/Quote/Events/HotelEventHandler.php
+++ Golfwny/Quote/Events/HotelEventHandler.php
@@ -1,6 +1,15 @@
 <?php namespace Quote\Events;
 
+use HotelRepositoryInterface;
+
 class HotelEventHandler {
+
+	protected $hotels;
+
+	public function __construct(HotelRepositoryInterface $hotels)
+	{
+		$this->hotels = $hotels;
+	}
 
 	public function onCreate($hotel)
 	{
@@ -9,7 +18,17 @@ class HotelEventHandler {
 
 	public function onDelete($hotel)
 	{
-		//
+		// Check to see if the hotel we're removing is the default. If it is,
+		// grab the first hotel we find for the region and set that hotel as
+		// the new default
+		if ((bool) $hotel->default === true)
+		{
+			// Get the first hotel for this region
+			$item = $this->hotels->getFirstBy('region_id', $hotel->region_id);
+
+			// Update the hotel
+			$this->hotels->update($item->id, ['default' => (int) true]);
+		}
 	}
 
 	public function onUpdate($hotel)
