66cd3f680bf165884a976817392bc0c905ce96a1
David VanScott <david.vanscott@gmail.com>
2015-01-20 09:53:04 -0500

    Remove the subheadings in the steps

--- Golfwny/Quote/views/pages/index.blade.php
+++ Golfwny/Quote/views/pages/index.blade.php
@@ -12,40 +12,24 @@
 			<i class="marker icon"></i>
 			<div class="content">
 				<div class="title">Location</div>
-				<div class="description">
-					@if ($location)
-						{{ $region->present()->name }}
-					@else
-						Choose your location
-					@endif
-				</div>
 			</div>
 		</div>
 		<div class="{{ $stepInfoStatus }} step">
 			<i class="info icon"></i>
 			<div class="content">
 				<div class="title">Basic Info</div>
-				<div class="description">
-					@if ($quote)
-						{{ $quote->present()->dates }}
-					@else
-						Tell us about your package
-					@endif
-				</div>
 			</div>
 		</div>
 		<div class="{{ $stepCoursesStatus }} step">
 			<i class="flag icon"></i>
 			<div class="content">
 				<div class="title">Courses</div>
-				<div class="description">Create your golf experience</div>
 			</div>
 		</div>
 		<div class="{{ $stepConfirmStatus }} step">
 			<i class="send icon"></i>
 			<div class="content">
 				<div class="title">Confirmation</div>
-				<div class="description">Review &amp; submit your quote</div>
 			</div>
 		</div>
 	</div>
