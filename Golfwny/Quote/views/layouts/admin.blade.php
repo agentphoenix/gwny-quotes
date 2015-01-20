057a58c47badbcf2fd5a1a0ae3d866b18d4ca412
David VanScott <david.vanscott@gmail.com>
2015-01-20 09:52:53 -0500

    Update to BS 3.3.2
    Also, cleanup the HTML a little bit

--- Golfwny/Quote/views/layouts/admin.blade.php
+++ Golfwny/Quote/views/layouts/admin.blade.php
@@ -15,7 +15,7 @@
 		@if (App::environment() == 'production')
 			<link href="//fonts.googleapis.com/css?family=Gruppo" rel="stylesheet">
 			<link href="//fonts.googleapis.com/css?family=Lato:300,400,400italic,700,900italic" rel="stylesheet">
-			<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
+			<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
 		@else
 			<link href="//localhost/global/bootstrap/3.3/css/bootstrap.min.css" rel="stylesheet">
 		@endif
@@ -90,54 +90,54 @@
 					&copy; {{ date('Y') }} Golf Western NY<br>Follow <a href="http://twitter.com/GolfWesternNY" target="_blank">@GolfWesternNY</a>
 				</footer>
 			</div>
-
-			@yield('modals')
-
-			@if (App::environment() == 'production')
-				<!--[if lt IE 9]>
-					<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-				<![endif]-->
-				<!--[if gte IE 9]><!-->
-					<script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
-				<!--<![endif]-->
-
-				<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-				<script src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.0/jquery.validate.min.js"></script>
-				<script src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.0/additional-methods.min.js"></script>
-				<script>
-					var _gaq = _gaq || [];
-					_gaq.push(['_setAccount', 'UA-39274726-1']);
-					_gaq.push(['_setDomainName', 'golfwny.com']);
-					_gaq.push(['_trackPageview']);
-
-					(function() {
-						var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-						ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-						var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-					})();
-				</script>
-			@else
-				<!--[if lt IE 9]>
-					<script src="//localhost/global/jquery/jquery-1.11.1.min.js"></script>
-				<![endif]-->
-				<!--[if gte IE 9]><!-->
-					<script src="//localhost/global/jquery/jquery-2.1.1.min.js"></script>
-				<!--<![endif]-->
-
-				<script src="//localhost/global/bootstrap/3.3/js/bootstrap.min.js"></script>
-				<script src="//localhost/global/jquery.validate/1.13/jquery.validate.min.js"></script>
-				<script src="//localhost/global/jquery.validate/1.13/additional-methods.min.js"></script>
-			@endif
-			{{ HTML::script('semantic/semantic.min.js') }}
-			<script>
-				$('.sidebar-toggle').on('click', function(e)
-				{
-					e.preventDefault();
-
-					$('.left.sidebar').sidebar('toggle');
-				});
-			</script>
-			@yield('scripts')
 		</div>
+
+		@yield('modals')
+
+		@if (App::environment() == 'production')
+			<!--[if lt IE 9]>
+				<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+			<![endif]-->
+			<!--[if gte IE 9]><!-->
+				<script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
+			<!--<![endif]-->
+
+			<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+			<script src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.0/jquery.validate.min.js"></script>
+			<script src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.0/additional-methods.min.js"></script>
+			<script>
+				var _gaq = _gaq || [];
+				_gaq.push(['_setAccount', 'UA-39274726-1']);
+				_gaq.push(['_setDomainName', 'golfwny.com']);
+				_gaq.push(['_trackPageview']);
+
+				(function() {
+					var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+					ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+					var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+				})();
+			</script>
+		@else
+			<!--[if lt IE 9]>
+				<script src="//localhost/global/jquery/jquery-1.11.1.min.js"></script>
+			<![endif]-->
+			<!--[if gte IE 9]><!-->
+				<script src="//localhost/global/jquery/jquery-2.1.1.min.js"></script>
+			<!--<![endif]-->
+
+			<script src="//localhost/global/bootstrap/3.3/js/bootstrap.min.js"></script>
+			<script src="//localhost/global/jquery.validate/1.13/jquery.validate.min.js"></script>
+			<script src="//localhost/global/jquery.validate/1.13/additional-methods.min.js"></script>
+		@endif
+		{{ HTML::script('semantic/semantic.min.js') }}
+		<script>
+			$('.sidebar-toggle').on('click', function(e)
+			{
+				e.preventDefault();
+
+				$('.left.sidebar').sidebar('toggle');
+			});
+		</script>
+		@yield('scripts')
 	</body>
 </html>
