6248a83b1a3f50ee0ec58207c30703cc5cf897b3
David VanScott <david.vanscott@gmail.com>
2015-01-26 09:50:08 -0500

    Show the accept/reject contract date

--- Golfwny/Quote/views/pages/package-contract-partial.blade.php
+++ Golfwny/Quote/views/pages/package-contract-partial.blade.php
@@ -1,1 +1,9 @@
 <h2>Contract</h2>
+
+@if ($quote->status == Status::CONTRACT_ACCEPTED)
+	<p><strong class="text-success">Contract Accepted:</strong> {{ $quote->present()->contractAccepted }}</p>
+@endif
+
+@if ($quote->status == Status::CONTRACT_REJECTED)
+	<p><strong class="text-danger">Contract Rejected:</strong> {{ $quote->present()->contractRejected }}</p>
+@endif
