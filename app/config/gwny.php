3577836309963aa364750f9339d795638e97e632
David VanScott <david.vanscott@gmail.com>
2015-01-26 09:48:58 -0500

    Add dates to the config

--- app/config/gwny.php
+++ app/config/gwny.php
@@ -2,6 +2,18 @@
 
 return [
 
+	'dates' => [
+		'date' => "l M jS, Y",
+		'dateNoDay' => "M jS, Y",
+		'dateFormal' => 'Y-m-d',
+		'day' => array(
+			'long' => "l",
+			'short' => "D",
+		),
+		'full' => "l M jS, Y, g:ia",
+		'time' => "g:ia",
+	],
+
 	'email' => [
 		'subject' => "[GolfWNY] ",
 		'to' => 'eric@golfwny.com',
