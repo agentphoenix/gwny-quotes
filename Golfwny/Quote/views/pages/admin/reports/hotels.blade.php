aa836ec9b550c24161e6c89ec9606e6d629d8433
David VanScott <david.vanscott@gmail.com>
2015-03-22 13:09:50 -0400

    Fix the hotel report display and years buttons

--- Golfwny/Quote/views/pages/admin/reports/hotels.blade.php
+++ Golfwny/Quote/views/pages/admin/reports/hotels.blade.php
@@ -10,7 +10,7 @@
 	<div class="btn-toolbar">
 		<div class="btn-group">
 		@foreach ($years as $year)
-			<a href="#" class="btn btn-default">{{ $year }}</a>
+			<a href="{{ route('admin.reports.hotels', [$year]) }}" class="btn btn-default">{{ $year }}</a>
 		@endforeach
 		</div>
 	</div>
@@ -18,25 +18,25 @@
 	@foreach ($revenue as $r)
 		<h2>{{ $r['name'] }} <small>{{ $r['region'] }}</small></h2>
 		<div class="row">
-			<div class="col-sm-4">
+			<div class="col-md-4">
 				<div class="ui statistic">
 					<div class="value" id="displayPrice">${{ number_format(round($r['revenue'], 2), 2) }}</div>
 					<div class="label">Total Revenue</div>
 				</div>
 			</div>
-			<div class="col-sm-4">
+			<div class="col-md-4">
 				<div class="ui statistic">
 					<div class="value" id="displayPrice">${{ number_format(round($r['tax'], 2), 2) }}</div>
 					<div class="label">Total Tax</div>
 				</div>
 			</div>
-			<div class="col-sm-2">
+			<div class="col-md-2">
 				<div class="ui statistic">
 					<div class="value" id="displayPrice">{{ $r['rooms'] }}</div>
 					<div class="label">Total Rooms</div>
 				</div>
 			</div>
-			<div class="col-sm-2">
+			<div class="col-md-2">
 				<div class="ui statistic">
 					<div class="value" id="displayPrice">{{ $r['people'] }}</div>
 					<div class="label">Total Guests</div>
