64aa56fd64e1fbe7e259f3b98542eb8f66f6779b
David VanScott <david.vanscott@gmail.com>
2015-03-22 12:48:56 -0400

    Fix some issues with the item presenter

--- Golfwny/Quote/Data/Presenters/QuoteItemPresenter.php
+++ Golfwny/Quote/Data/Presenters/QuoteItemPresenter.php
@@ -7,7 +7,11 @@ class QuoteItemPresenter extends Presenter {
 
 	public function address()
 	{
-		return nl2br($this->entity->address);
+		if ( ! empty($this->entity->hotel_id))
+			return nl2br($this->entity->hotel->present()->address);
+
+		if ( ! empty($this->entity->course_id))
+			return nl2br($this->entity->course->present()->address);
 	}
 
 	public function hotel()
@@ -22,7 +26,11 @@ class QuoteItemPresenter extends Presenter {
 
 	public function gm()
 	{
-		return $this->entity->general_manager;
+		if ( ! empty($this->entity->hotel_id))
+			return nl2br($this->entity->hotel->present()->general_manager);
+
+		if ( ! empty($this->entity->course_id))
+			return nl2br($this->entity->course->present()->general_manager);
 	}
 
 	public function people()
@@ -37,9 +45,20 @@ class QuoteItemPresenter extends Presenter {
 		return "{$people} people";
 	}
 
+	public function peopleRaw()
+	{
+		if ($this->entity->people) return $this->entity->people;
+
+		return false;
+	}
+
 	public function phone()
 	{
-		return $this->entity->phone;
+		if ( ! empty($this->entity->hotel_id))
+			return nl2br($this->entity->hotel->present()->phone);
+
+		if ( ! empty($this->entity->course_id))
+			return nl2br($this->entity->course->present()->phone);
 	}
 
 	public function rate()
@@ -96,6 +115,16 @@ class QuoteItemPresenter extends Presenter {
 	public function holes()
 	{
 		return $this->entity->holes." holes";
+	}
+
+	public function holesRaw()
+	{
+		return $this->entity->holes;
+	}
+
+	public function totalRounds()
+	{
+		return $this->peopleRaw() * ($this->entity->holes / 18);
 	}
 
 }
