a6b2034de15285af0fbb98fc5a4353dee2728fc0
David VanScott <david.vanscott@gmail.com>
2015-03-29 10:14:41 -0400

    Reminder for Eric to send deposit email

--- Golfwny/Quote/views/emails/admin/contract-accepted.blade.php
+++ Golfwny/Quote/views/emails/admin/contract-accepted.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.email')
 
 @section('content')
-	<p>{{ $fromName }} has accepted the contract.</p>
+	<p>{{ $fromName }} has accepted the contract. Use the name and email address below to contact the submitter and collect the package deposit ASAP.</p>
+
+	<p><strong>Name:</strong> {{ $fromName }}</p>
+	<p><strong>Email Address:</strong> {{ $fromEmail }}</p>
 @stop
