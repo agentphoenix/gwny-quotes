6b191ff5067b6ee65079141a6dfe1f646dc2ffe1
David VanScott <david.vanscott@gmail.com>
2015-03-29 14:20:17 -0400

    Update estimate email

--- Golfwny/Quote/views/emails/customer/estimate.blade.php
+++ Golfwny/Quote/views/emails/customer/estimate.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 	<p>Dear {{ $name }},</p>
 
-	<p>We would like to let you know that your quote for your Stay N Play Golf Package is available for review. Once reviewed please approve or reject this quote. If you have any questions please feel free to contact us anytime.</p>
+	<p>We would like to let you know that your quote for your Stay N Play Golf Package is available for review. Once reviewed please approve or reject this quote. We will beat any competitor's pricing on a comparable golf package. If you have any questions please feel free to contact us anytime.</p>
 
 	<p><a href="{{ route('package', [$code]) }}">Your Package Information</a></p>
 
