fb7c7a1ca7981665c91f9b60a4de3562e5320bbb
David VanScott <david.vanscott@gmail.com>
2015-06-18 16:01:38 -0400

    Add note about checking spam folder

--- Golfwny/Quote/views/pages/thank-you.blade.php
+++ Golfwny/Quote/views/pages/thank-you.blade.php
@@ -8,4 +8,6 @@
 	<h1>Thank You!</h1>
 
 	<p>Thank you for your submission. We'll look over this and get back to you shortly with an estimate for your package.</p>
+
+	{{ alert('info', "Note: if you don't see any emails from us, please check your <em>Junk</em> folder as emails from our system can sometimes be classified as spam by some email providers.") }}
 @stop
