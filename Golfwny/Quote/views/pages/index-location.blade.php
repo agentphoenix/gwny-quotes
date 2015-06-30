175234b62d6c1198e9d689f786ef129bc6676008
David VanScott <david.vanscott@gmail.com>
2015-06-30 14:21:55 -0400

    Updated region images

--- Golfwny/Quote/views/pages/index-location.blade.php
+++ Golfwny/Quote/views/pages/index-location.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-	<div class="col-md-4">
+	<div class="col-md-6">
 		<div class="panel panel-default bootcards-media">
 			<div class="panel-heading">
 				<h3 class="panel-title">Rochester, NY</h3>
@@ -28,7 +28,7 @@
 		</div>
 	</div>
 
-	<div class="col-md-4">
+	<div class="col-md-6">
 		<div class="panel panel-default bootcards-media">
 			<div class="panel-heading">
 				<h3 class="panel-title">Buffalo, NY</h3>
@@ -57,7 +57,7 @@
 		</div>
 	</div>
 
-	<div class="col-md-4">
+	<div class="col-md-6">
 		<div class="panel panel-default bootcards-media">
 			<div class="panel-heading">
 				<h3 class="panel-title">Silver Lake Resort</h3>
@@ -71,6 +71,35 @@
 						</div>
 						<div class="hidden-xs">
 							<a href="http://golfwny.com/rochester/course/silver-lake-country-club" target="_blank" class="btn btn-default btn-block">More Info</a>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="visible-xs">
+							<p><a href="http://golfwny.com/rochester/book-package" target="_blank" class="btn btn-primary btn-lg btn-block">Get Started</a></p>
+						</div>
+						<div class="hidden-xs">
+							<a href="http://golfwny.com/rochester/book-package" target="_blank" class="btn btn-primary btn-block">Get Started</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-md-6">
+		<div class="panel panel-default bootcards-media">
+			<div class="panel-heading">
+				<h3 class="panel-title">Reservoir Creek</h3>
+			</div>
+			{{ HTML::image('img/reservoircreek.jpg', null, ['class' => 'img-responsive']) }}
+			<div class="panel-footer">
+				<div class="row">
+					<div class="col-sm-6">
+						<div class="visible-xs">
+							<p><a href="http://golfwny.com/rochester/course/reservoir-creek-golf-course--inn" target="_blank" class="btn btn-default btn-lg btn-block">More Info</a></p>
+						</div>
+						<div class="hidden-xs">
+							<a href="http://golfwny.com/rochester/course/reservoir-creek-golf-course--inn" target="_blank" class="btn btn-default btn-block">More Info</a>
 						</div>
 					</div>
 					<div class="col-sm-6">
